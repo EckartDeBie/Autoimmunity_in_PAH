5f215189f61765fe99592f17cd7c032bc952df3b
EckartDeBie <83496949+EckartDeBie@users.noreply.github.com>
2021-08-09 19:03:24 +0200

    updated folder names of data directory

--- Autoimmunity_inPAH_script_forGitHub.R
+++ Autoimmunity_inPAH_script_forGitHub.R
@@ -21,7 +21,7 @@ set.seed(42) # reproducility, not sure if matchit uses random
 #First step match the cases and controls
 
 
-data_dir="~/My publications/data re-run code" # where all the data are stored
+data_dir="~/My publications/paper/data re-run code" # where all the data are stored
 data.Eoin.v1.4March <- read.csv2(file.path(data_dir,"data_Eoin_v1_4March.csv"), stringsAsFactors=FALSE)
 autoimmune_profiling_2nd_batch_5_missing_samples_removed <-
   read_excel(file.path(data_dir,"autoimmune_profiling_2nd_batch__5_missing_samples_removed.xlsx")) %>%
